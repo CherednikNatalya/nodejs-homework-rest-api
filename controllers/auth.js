4365152e603f60c6fdde235adc843ebbe484877d
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-06-03 13:20:59 +0300

    add avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,14 @@
 const bcrypt =  require('bcrypt');
 const jwt = require('jsonwebtoken');
+const gravatar =require('gravatar');
+const path = require('path');
+const fs = require('fs').promises;
+
+
 const {User, subscriptionList} = require('../models/user');
 const { HttpError, ctrlWrapper } = require("../helpers");
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
 const {email, password} =req.body;
@@ -12,8 +19,9 @@ if(user) {
 }
 
 const hashPassword = await bcrypt.hash(password, 10);
+const avatarURL = gravatar.url(email);
     
-const newUser = await User.create({...req.body, password:hashPassword});
+const newUser = await User.create({...req.body, password:hashPassword, avatarURL});
 
 res.status(201).json({
     email: newUser.email,
@@ -63,6 +71,20 @@ const logout = async(req, res) => {
 
 }
 
+const updateAvatar = async(req, res)=> {
+  const {_id} = req.user;
+  const {path: tempUpload, originalname} = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, {avatarURL});
+
+  res.json({
+      avatarURL,
+  })
+}
+
 const updateSubscription = async (req, res) => {
     const errorSubscription = new HttpError(400, "Invalid subscription value");
     const { subscription } = req.body;
@@ -91,5 +113,6 @@ module.exports = {
     getCurrent:ctrlWrapper(getCurrent),
     logout:ctrlWrapper(logout),
     updateSubscription :ctrlWrapper(updateSubscription ),
+    updateAvatar: ctrlWrapper(updateAvatar),
   };
   
